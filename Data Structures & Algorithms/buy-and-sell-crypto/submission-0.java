41222b605e3004db7782d7763807c1b89b0323b6
Виктория Кузовлева <118015768+AstVic@users.noreply.github.com>
2026-08-31 13:40:46 +0300

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int curMin = prices[0];
+        int res = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int curProfit = prices[i] - curMin;
+            res = Integer.max(res, curProfit);
+            curMin = Integer.min(curMin, prices[i]);
+        }
+
+        return res;
+    }
+}
